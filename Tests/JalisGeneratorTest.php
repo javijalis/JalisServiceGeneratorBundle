7f325618ad6e3cdb0904ae08394f38d7777e90f2
Javier Sanchez <javijalis@gmail.com>
2013-01-03 13:35:00 +0100

    test

---
+++ Tests/JalisGeneratorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Jalis\Bundle\ServiceGeneratorBundle\Tests;
+
+
+class JalisGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+
+      $this->assertTrue(true);
+    }
+}
